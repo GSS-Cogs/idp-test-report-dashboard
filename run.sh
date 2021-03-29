862b46299675d6d473c1a7e228959b318ce9f298
Michael Adams <mikelovesbooks@gmail.com>
2021-03-29 20:02:33 +0100

    update to reflect tweaks I made on the box

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-python3 get_data.py
-docker run -p 5050:5050 -e KEEP_HISTORY=1 \
+# Note you'll need to pipenv install first
+pipenv run python3 get_data.py
+
+docker run -d -p 5050:5050 -e KEEP_HISTORY=1 \
                  -v ${PWD}/allure-results:/app/allure-results \
                  -v ${PWD}/allure-report:/app/default-reports \
                  frankescobar/allure-docker-service
